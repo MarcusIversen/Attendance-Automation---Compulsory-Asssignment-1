f7268782c9ae089a9e0a6d4a29fedbe9a00895e2
KristianHollaender <kristianhansen9@hotmail.dk>
2022-02-02 13:08:59 +0100

    push

--- src/gui/controller/MainMenuController.java
+++ src/gui/controller/MainMenuController.java
@@ -1,5 +1,14 @@
 package gui.controller;
 
-public class MainMenuController {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
