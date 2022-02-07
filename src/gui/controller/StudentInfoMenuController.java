2c765f8fb34aa6ff688c5a973f11eebdc673c7da
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2022-02-07 10:46:19 +0100

    .

--- src/gui/controller/StudentInfoMenuController.java
+++ src/gui/controller/StudentInfoMenuController.java
@@ -46,11 +46,11 @@ public class StudentInfoMenuController implements Initializable {
 
     // add your data here from any source
     private ObservableList<Absence> absence = FXCollections.observableArrayList(
-            new Absence("Monday", "10 %"),
-            new Absence("Tuesday", "15 %"),
-            new Absence("Wednesday", "15 %"),
+            new Absence("Monday", "80 %"),
+            new Absence("Tuesday", "60 %"),
+            new Absence("Wednesday", "40 %"),
             new Absence("Thursday", "15 %"),
-            new Absence("Friday", "15 %")
+            new Absence("Friday", "5 %")
     );
 
     private PieChart buildPieChart() {
