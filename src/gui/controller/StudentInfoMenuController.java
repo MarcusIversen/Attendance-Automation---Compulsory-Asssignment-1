a84d81083b54627b60c5fb27d851bc64c02f2c7c
Ferniss <mathiasf2000@hotmail.com>
2022-02-09 12:39:28 +0100

    Logout from student works

--- src/gui/controller/StudentInfoMenuController.java
+++ src/gui/controller/StudentInfoMenuController.java
@@ -5,19 +5,25 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StudentInfoMenuController implements Initializable {
-
+    @FXML
+    private Button btnLogOut;
     @FXML
     private Button checkInBtn;
     @FXML
@@ -82,5 +88,12 @@ public class StudentInfoMenuController implements Initializable {
             checkInBtn.setText("You are now checked out!");
             hasCheckedIn = true;
         }
+    }
+
+    public void goLogout(ActionEvent actionEvent) throws IOException {
+        Stage switcher = (Stage) btnLogOut.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/view/LoginMenu.fxml"));
+        Scene scene = new Scene(root);
+        switcher.setScene(scene);
     }
 }
