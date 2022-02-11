b8a301a2b0e1e2217381e3eaa95aee8c6886ddff
Ferniss <mathiasf2000@hotmail.com>
2022-02-11 11:30:43 +0100

    Data modified (Last push)

--- src/dal/ChartDAO.java
+++ src/dal/ChartDAO.java
@@ -37,19 +37,19 @@ public class ChartDAO {
     }
     public XYChart.Series getBarDataB(){
         XYChart.Series data = new XYChart.Series();
-        data.getData().add(new XYChart.Data("Kim Jong Un", 80 ));
-        data.getData().add(new XYChart.Data("Henrik Henriksen", 60.54));
-        data.getData().add(new XYChart.Data("John Johnson", 45.21));
-        data.getData().add(new XYChart.Data("Andy Lam", 20.42));
-        data.getData().add(new XYChart.Data("Theis Andersen", 24.12));
-        data.getData().add(new XYChart.Data("Anders Andersen", 56.23));
-        data.getData().add(new XYChart.Data("Trine Thomsen", 76.42));
-        data.getData().add(new XYChart.Data("Hans Vestergaard", 1.02));
-        data.getData().add(new XYChart.Data("Kristian Hollænder", 6));
-        data.getData().add(new XYChart.Data("Alexander Christensen", 9));
-        data.getData().add(new XYChart.Data("Peter Petersen", 3.45));
-        data.getData().add(new XYChart.Data("Jens Jensen", 23.65));
-        data.getData().add(new XYChart.Data("Mathias Fernissssss", 77.42));
+        data.getData().add(new XYChart.Data("Vladimir Putin", 20 ));
+        data.getData().add(new XYChart.Data("Ole hans", 30.54));
+        data.getData().add(new XYChart.Data("Emil Lindberg", 50.21));
+        data.getData().add(new XYChart.Data("Bambi Short", 80.42));
+        data.getData().add(new XYChart.Data("Jensen Jensen", 44.12));
+        data.getData().add(new XYChart.Data("Rasmus Rasmussen", 26.23));
+        data.getData().add(new XYChart.Data("Line Thomsen", 46.42));
+        data.getData().add(new XYChart.Data("Camilla Westergaard", 8.02));
+        data.getData().add(new XYChart.Data("Hans Hollander", 60.05));
+        data.getData().add(new XYChart.Data("Julie Jul", 15.46));
+        data.getData().add(new XYChart.Data("Peter Pan", 90.35));
+        data.getData().add(new XYChart.Data("Egon Olsen", 99.99));
+        data.getData().add(new XYChart.Data("Benny kel", 69.69));
         return data;
     }
 }
--- src/dal/StudentDAO.java
+++ src/dal/StudentDAO.java
@@ -29,20 +29,20 @@ public class StudentDAO {
     }
     public List<Student> getStudentsB(){
         ObservableList<Student> students = FXCollections.observableArrayList(
-                new Student("Kim Jong Un", "Yes", "80.00 %"),
-                new Student("Henrik Henriksen", "No", "60.54 %"),
-                new Student("John Johnson", "No", "45.21 %"),
-                new Student("Andy Lam", "No", "20.42 %"),
-                new Student("Theis Andersen", "No", "24.12 %"),
-                new Student("Anders Andersen", "No", "56.23 %"),
-                new Student("Trine Thomsen", "No", "76.42 %"),
-                new Student("Lone Hansen", "No", "4.02 %"),
-                new Student("Hans Vestergaard", "No", "1.02 %"),
-                new Student("Kristian Hollænder", "No", "6 %"),
-                new Student("Alexander Christensen", "No", "9 %"),
-                new Student("Peter Petersen", "No", "3.45 %"),
-                new Student("Jens Jensen", "No", "23.65 %"),
-                new Student("Mathias Fernisssss", "No", "77.42 %")
+                new Student("Vladimir Putin", "No", "20.00 %"),
+                new Student("Ole hans", "Yes", "30.54 %"),
+                new Student("Emil Lindberg", "Yes", "50.21 %"),
+                new Student("Bambi Short", "Yes", "80.42 %"),
+                new Student("Jensen Jensen", "Yes", "44.12 %"),
+                new Student("Rasmus Rasmussen", "No", "26.23 %"),
+                new Student("Line Thomsen", "Yes", "46.42 %"),
+                new Student("Camilla Westergaard", "Yes", "8.02 %"),
+                new Student("Hans Hollander", "Yes", "60.05 %"),
+                new Student("Julie Jul", "Yes", "15.46 %"),
+                new Student("Peter Pan", "Yes", "9 %"),
+                new Student("Peter Petersen", "No", "90.35 %"),
+                new Student("Egon Olsen", "Yes", "99.99 %"),
+                new Student("Benny kel", "Yes", "69.69 %")
         );
         return students;
     }
